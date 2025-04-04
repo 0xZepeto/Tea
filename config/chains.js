a472051e11dd9f886e3a592a761a346ea57c22d6
0xZepeto <72789671+0xZepeto@users.noreply.github.com>
2025-04-04 21:17:17 +0700

    Update chains.js

--- config/chains.js
+++ config/chains.js
@@ -1,1 +1,11 @@
-
+config/chains.js
+export const chains = [
+  {
+    name: "Tea Sepolia",
+    rpcUrl: "https://tea-sepolia.g.alchemy.com/v2/vot1HrCuj9CmekoM2FKNnv6h4Mnzjyb_",
+    chainId: "10218",
+    symbol: "TEA",
+    explorer: "https://sepolia.tea.xyz",
+    isMainnet: false
+  }
+];
